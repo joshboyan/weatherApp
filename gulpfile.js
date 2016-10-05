feab5366e9f28a9f398c61d6cf1d0bdbe2e48fc7
joshboyan <joshboyan@yahoo.com>
2016-10-05 10:25:28 -0700

    Add JS concatenation

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var coffee = require('gulp-coffee');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
+
+var coffeeSources = ['components/coffee/*.coffee'];
+var jsSources = ['components/scripts/*.js']; //may need to dictate specific concatenation order
 
 gulp.task('coffee', function() {
 	gulp.src('components/coffee/tagline.coffee')
 	.pipe(coffee({bare: true})
 		.on('error', gutil.log))
 	.pipe(gulp.dest('components/scripts'))
-		)
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+	.pipe(concat('scripts.js'))
+	.pipe(gulp.dest('builds/dev/js'))
 });
