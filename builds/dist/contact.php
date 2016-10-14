0d4a3245588886bed644ccd26d13d1d81b6fcc20
joshboyan <joshboyan@yahoo.com>
2016-10-14 01:09:30 -0700

    Finish tutorial.html markup

--- builds/dist/contact.php
+++ builds/dist/contact.php
@@ -1,12 +1,33 @@
+<!-- This is the HTML form that is being processed by this php file. It is meant to be a guide but if you copy and paste it into your HTML it will work.
+
+<form name="Form" action="contact_process.php" method="post">
+	<label for="email" id="email">Email</label>
+	<input type="text" name="email" required>
+	<select name="subject">
+  	<option value="help">Build me a site!</option>
+  	<option value="suggestion">I have a suggestion.</option>
+  	<option value="question">I have a question.</option>
+  	<option value="issue">I found an issue.</option>
+	</select>
+	<label for="name">Name</label>
+	<input type="text" name="name" required>	
+	<label for="subject">What's Up?</label>
+	<label for="message">Tell me more...</label>
+	<textarea name="message" rows="5" required></textarea>
+	<input type="submit" name="submit" value="Submit">
+	</form>
+
+-->
+
 <?php
 
-// Gets posted data from the HTML form fields and creates local variables. The items with the ' marks around them are the name values from the fields in the HTML form example above. Note, the first three variables are required for all email messages (as described above).
+// Gets posted data from the HTML form fields and creates local variables. The items with the ' marks around them are the name values from the fields in the HTML form. Note, the first three variables are required for all email messages.
 
 $EmailFrom = Trim(stripslashes($_POST['email'])); 
 $EmailTo = "joshboyan@yahoo.com";
 $Subject = Trim(stripslashes($_POST['subject'])); 
 $Name = Trim(stripslashes($_POST['name'])); 
-$Comments = Trim(stripslashes($_POST['comments'])); 
+$Message = Trim(stripslashes($_POST['message'])); 
 $current_date = date("Y-m-d");
  // This date is created when the form is submitted.
 
@@ -19,10 +40,13 @@ $current_date = date("Y-m-d");
 // The fields being validated here, from the form example above, are the email and name fields. Those must contain some form of data for the PHP to accept them, otherwise the error.html page is generated to the form user.
 
 $validationOK=true;
-if (Trim($EmailFrom)=="") $validationOK=false;
+if (!filter_var(Trim($EmailFrom), FILTER_VALIDATE_EMAIL)) {
+    $validationOK=false;
+} 
 if (Trim($Name)=="") $validationOK=false;
 if (!$validationOK) {
-print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+//print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+echo "<script type='text/javascript'>alert('It seems your email address is invalid!');</script>";
 exit;
 }
 
@@ -39,7 +63,7 @@ exit;
 $myFilePath = "contacts/";
 $myFileName = "form-data.csv";
 $myPointer = fopen ($myFilePath.$myFileName, "a+");
-$form_data = $current_date . "," . $EmailFrom . "," . $Name . "," . $Date . "," . $Tours . "," . $Comments . "\n";
+$form_data = $current_date . "," . $EmailFrom . "," . $Name . "," . $Date . "," . "," . $Message . "\n";
 fputs ($myPointer, $form_data);
 fclose ($myPointer);
 
@@ -47,7 +71,7 @@ fclose ($myPointer);
 // This section of PHP prepares the email body text. This is the fourth and final required element to compose and send an email message from a server-side script. 
 
 $Body = "";
-$Body .= $Comments;
+$Body .= $Message;
 $Body .= "\n";
 
 // Note -- The ".=" means to append to (added to) the previous variable. So there is only one $Body variable, and all the other parts are appended to that one. The "\n" means to place a hard return between these lines in the email message. If the "\n" weren't included, all the items would be run together on one long line.
@@ -59,12 +83,14 @@ $success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
 // If the page validates and there are no errors in the PHP, this line redirect to ok.html page, which is the "success page" for the form submission.
 
 if ($success){
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=ok.html\">";
+  //print "<meta http-equiv=\"refresh\" content=\"0;URL=ok.html\">";
+  echo "<script type='text/javascript'>alert('Message has been successfully sent!');</script>";
 }
 else{
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+  //print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+  echo "<script type='text/javascript'>alert('There was an error with the message!');</script>";
 }
-
+//Should I change these two meta tags to HTML5: <meta charset="UTF-8">?
 
 ?>
 
