2e4fecbaf3926b8a689276aa85577108ba19c6c9
joshboyan <joshboyan@yahoo.com>
2017-03-02 17:54:14 -0800

    Add standard files to filesToCacheVariable

--- components/sw.js
+++ components/sw.js
@@ -1,5 +1,12 @@
-var cacheName = 'app-cache-v1';
-var filesToCache = ['./index.html'];
+// Once your project goes live this 
+var cacheName = 'project-cache-v1';
+var filesToCache = [
+	'/',
+	'/index.html',
+	'/css/styles.css',
+	'/js/scripts.js',
+	'/img/favicon.png'
+	];
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
@@ -11,3 +18,26 @@ self.addEventListener('install', function(e) {
   );
 });
 
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
